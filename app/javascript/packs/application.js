788bb54fdd7f125d7f4354ac062fea2968f72968
phongtv-1971 <truong.van.phong@sun-asterisk.com>
2022-06-24 16:32:41 +0700

    init demo

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
